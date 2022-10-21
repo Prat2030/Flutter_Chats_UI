a585797f708a9c42bd768aebc7533ebf81fdd49f
Prat2030 <pratyushjindal2003@gmail.com>
2022-10-21 20:10:57 +0530

    completed favorites Contact widget and imported asset images.

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,6 +1,7 @@
-// ignore_for_file: prefer_const_constructors
+// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
 
 import 'package:flutter/material.dart';
+import 'package:flutter_chats_ui/widgets/favourite_contacts.dart';
 import '../widgets/category_selector.dart';
 
 class HomeScreen extends StatefulWidget {
@@ -12,6 +13,7 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Theme.of(context).primaryColor,
       appBar: AppBar(
         leading: IconButton(
           icon: Icon(Icons.menu),
@@ -39,6 +41,22 @@ class _HomeScreenState extends State<HomeScreen> {
       body: Column(
         children: <Widget>[
           CategorySelector(),
+          Expanded(
+            child: Container(
+              decoration: BoxDecoration(
+                color: Theme.of(context).accentColor,
+                borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(30.0),
+                  topRight: Radius.circular(30.0),
+                ),
+              ),
+              child: Column(
+                children: <Widget>[
+                  FavoriteContacts(),
+                ],
+              ),
+            ),
+          ),
         ],
       ),
     );
